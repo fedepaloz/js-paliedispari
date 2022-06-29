21bd328b9361acf7ce69f380635cb61f15c8ffef
fedepaloz <palozzifederico@gmail.com>
2022-06-29 16:36:13 +0200

    funzione prova

---
+++ palindromo/script.js
@@ -0,0 +1,25 @@
+/* Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma */
+
+/* // ?1 - Come la chiamo?
+// ?2 - Ho bisogno di parametri? (se si quali?)
+// ?3 - Devo restituire qualcosa? (se si cosa? di che tipo?)
+// ?4 OPZIONALE:  Qualcosa  potrebbe andare storto? (se si, cosa?) */
+
+
+//creo un prompt nel quale chiedo la parola da inserire
+
+let inputWord = prompt("inserisci una parola" , "giraffa")
+
+
+
+function reverseWord(word) {
+    let result = '';
+    for (let i = word.length - 1; i >= 0; i--) { result += word[i]; }
+
+    return result;
+}
+
+const reversedWord = reverseWord("giraffa");
+
+console.log(reversedWord);
